45edf05d56f6febb8631ba9ce3f8cbefe3e55234
Jason <jason223535@gmail.com>
2025-09-27 22:05:38 +0800

    Add CategoryMapper

---
+++ src/main/java/com/gtelant/commerce_service/mappers/CategoryMapper.java
@@ -0,0 +1,24 @@
+package com.gtelant.commerce_service.mappers;
+
+import com.gtelant.commerce_service.dtos.CategoryRequest;
+import com.gtelant.commerce_service.dtos.CategoryResponse;
+import com.gtelant.commerce_service.models.Category;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryMapper {
+    public CategoryResponse toCategoryResponse(Category category) {
+        CategoryResponse dto = new CategoryResponse();
+        dto.setId(category.getId());
+        dto.setName(category.getName());
+        return dto;
+    }
+
+    public Category toCategory(CategoryRequest dto) {
+        Category category = new Category();
+        category.setName(dto.getName());
+
+        return category;
+    }
+
+}
