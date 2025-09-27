43c9fbbdd1fa13d3035950d99978da1928e59857
Jason <jason223535@gmail.com>
2025-09-27 22:14:22 +0800

    Add UpdateReviewStatusRequest

---
+++ src/main/java/com/gtelant/commerce_service/dtos/UpdateReviewStatusRequest.java
@@ -0,0 +1,13 @@
+
+package com.gtelant.commerce_service.dtos;
+
+import com.gtelant.commerce_service.enums.ReviewStatus;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UpdateReviewStatusRequest {
+    private List<Integer> ids;
+    private ReviewStatus reviewStatus;
+}
