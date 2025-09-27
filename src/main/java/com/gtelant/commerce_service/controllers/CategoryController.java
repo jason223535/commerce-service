f27b64cd4b10f81711f5701abc1ea6731794c070
Jason <jason223535@gmail.com>
2025-09-27 22:05:20 +0800

    Add CategoryController

---
+++ src/main/java/com/gtelant/commerce_service/controllers/CategoryController.java
@@ -0,0 +1,58 @@
+package com.gtelant.commerce_service.controllers;
+
+import com.gtelant.commerce_service.dtos.*;
+import com.gtelant.commerce_service.mappers.CategoryMapper;
+import com.gtelant.commerce_service.mappers.ProductMapper;
+import com.gtelant.commerce_service.models.Category;
+import com.gtelant.commerce_service.models.User;
+import com.gtelant.commerce_service.services.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+@Tag(name = "Category", description = "Category management APIs")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    @Autowired
+    private ProductMapper productMapper;
+
+    @Operation(summary = "Get all categories", description = "Returns a list of all categories")
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> getAllCategories() {
+        List<CategoryResponse> responses= categoryService.getAllCategories().stream()
+                .map(category -> {
+                    CategoryResponse response = categoryMapper.toCategoryResponse(category);
+                    List<CategoryProductResponse> productResponseList = category
+                            .getProductList()
+                            .stream()
+                            .map(product -> productMapper.toCategoryProductResponse(product)).toList();
+                    response.setProductList(productResponseList);
+                    return response;
+                })
+                .toList();
+
+        return ResponseEntity.ok(responses);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryResponse> updateCategory(@PathVariable int id, @RequestBody CategoryRequest categoryRequest) {
+        Category category = categoryMapper.toCategory(categoryRequest);
+        Category updatedCategory = categoryService.updateCategory(id, category);
+        if (updatedCategory == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(categoryMapper.toCategoryResponse(updatedCategory));
+    }
+}
